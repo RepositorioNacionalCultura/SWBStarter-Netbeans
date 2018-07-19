f55170110d16ee462ad320975de61e44a31b3363
renejara-infotec <rene.jara@infotec.mx>
2018-07-18 22:59:56 -0500

    Version parcial de registro de anotadores

--- Components/src/main/java/mx/gob/cultura/portal/resources/UserRegistry.java
+++ Components/src/main/java/mx/gob/cultura/portal/resources/UserRegistry.java
@@ -1,6 +1,7 @@
 package mx.gob.cultura.portal.resources;
 
 import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.Property;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.SocketException;
@@ -21,69 +22,122 @@ import org.semanticwb.model.UserRepository;
 import org.semanticwb.model.WebPage;
 import org.semanticwb.platform.SemanticObject;
 import org.semanticwb.portal.SWBSessionObject;
-import org.semanticwb.portal.api.GenericResource;
+import org.semanticwb.portal.api.GenericAdmResource;
 import org.semanticwb.portal.api.SWBActionResponse;
 import org.semanticwb.portal.api.SWBParamRequest;
 import org.semanticwb.portal.api.SWBResourceException;
+import org.semanticwb.portal.api.SWBResourceURL;
+import org.semanticwb.portal.api.SWBResourceURLImp;
 
 /**
  * Realiza la visualizacion y cambios en la informacion del perfil de usuarios
  * @author jose.jimenez
  */
-public class UserRegistry extends GenericResource {
+public class UserRegistry extends GenericAdmResource {
     
     
     private static final Logger LOG = SWBUtils.getLogger(UserRegistry.class);
     
-    public static final String REGISTER_ACTION = "creating";
-    
+    public static final String ACTION_REGISTER = "creating";
+    public static final String REGISTER_ACTION = "creating";//legacy
+        
     public static final String REGISTER_MODE = "confirmRegistry";
+    
+    public static final String ACTION_BE_ANNOTATOR = "beannotator";
+    
+    public static final String ACTION_CONFIRMING = "confirming";
     
     private String confirmationActionUrl = null;
 
     private static final String DATACRED_URI =
             "http://www.semanticwebbuilder.org/swb4/ontology#plainCredential";
+    public static final String ORGANITATION_NAME_URI =
+            "http://www.semanticwebbuilder.org/swb4/ontology#organitationName";
+    public static final String POSITION_TITLE_URI =
+            "http://www.semanticwebbuilder.org/swb4/ontology#positionTitle";
+    public static final String TELEPHONE_NUMBER_URI =
+            "http://www.semanticwebbuilder.org/swb4/ontology#telephoneNumber";
+    public static final String AREA_INTEREST_URI =
+            "http://www.semanticwebbuilder.org/swb4/ontology#areaInterest";
     
+    private UserRepository userRepository;
+    
+    @Override
+    public void init() throws SWBResourceException {
+        super.init(); 
+        userRepository=getResourceBase().getWebSite().getUserRepository();        
+    }
+    
+    @Override
     public void doView(HttpServletRequest request, HttpServletResponse response,
             SWBParamRequest paramsRequest) throws IOException {
         
-        String url = "/swbadmin/jsp/rnc/userRegistry.jsp";
+        //String url = "/swbadmin/jsp/rnc/userRegistry.jsp";
+        String url = "/swbadmin/jsp/rnc/"+this.getClass().getSimpleName()+"/view.jsp";
+        User user = paramsRequest.getUser();
+        boolean isAnnotator= false;
+        if (user!=null && user.isSigned()){
+            isAnnotator=user.hasRole(userRepository.getRole(this.getResourceBase().getAttribute("AnnRol", "")));          
+        }
         RequestDispatcher rd = request.getRequestDispatcher(url);
         try {
             request.setAttribute("paramsRequest", paramsRequest);
+            request.setAttribute("isAnnotator", isAnnotator);
             rd.include(request, response);
         } catch(ServletException se) {
             UserRegistry.LOG.info(se.getMessage());
         }
-        if (null == this.confirmationActionUrl) {
-            String serverUrl = request.getRequestURL().substring(0, request.getRequestURL().indexOf("/", 8));
+/*        if (null == this.confirmationActionUrl) {
+            String serverUrl = request.getScheme() + "://" + request.getServerName() + ((request.getServerPort() != 80)? (":" + request.getServerPort()) : "");
             this.confirmationActionUrl = serverUrl + paramsRequest.getActionUrl().setAction("confirming").toString();
-        }
+        }*/
     }
 
     @Override
     public void processAction(HttpServletRequest request, SWBActionResponse response)
             throws SWBResourceException, IOException {
-        
+
         String action = response.getAction();
+//System.out.println("-----------------------------------------ProcessAction:"+action);        
         String alert = null;
-        UserRepository userRepo = response.getWebPage().getWebSite().getUserRepository();
         String nextMode = null;
         System.out.println("Referer: " + request.getHeader("Referer"));
         
-        if (UserRegistry.REGISTER_ACTION.equals(action)) {
-            User created = this.createProfile(request, userRepo);
+        if (ACTION_REGISTER.equals(action)) {
+            User created = this.createProfile(request);
             if (null == created) {
                 response.setRenderParameter("condition",
                         (String) request.getAttribute("condition"));
                 System.out.println("USUARIO NO CREADO");
             } else {
+                if (null == this.confirmationActionUrl) {
+                    String serverUrl = request.getScheme() + "://" + request.getServerName() + ((request.getServerPort() != 80)? (":" + request.getServerPort()) : "");
+                    SWBResourceURLImp urlAcc = new SWBResourceURLImp(request, this.getResourceBase(), response.getWebPage(), SWBResourceURL.UrlType_ACTION);
+                    this.confirmationActionUrl = serverUrl + urlAcc.setAction("confirming").toString();
+                }               
                 this.sendConfirmationEmail(created);
                 System.out.println("Se creo, el usuario  >>>");
             }
             nextMode = UserRegistry.REGISTER_MODE;
-        } else if ("confirming".equals(action)) {
-            User user = this.activateUser(request, userRepo);
+        }else if (response.Action_EDIT.equals(action)) {
+            User updated = this.updateProfile(request, response.getUser());
+            if (null == updated) {
+                response.setRenderParameter("condition",
+                        (String) request.getAttribute("condition"));
+                System.out.println("USUARIO NO actualizado");
+            } /*else {
+             
+            if (null == this.confirmationActionUrl) {
+                    String serverUrl = request.getScheme() + "://" + request.getServerName() + ((request.getServerPort() != 80)? (":" + request.getServerPort()) : "");
+                    SWBResourceURLImp urlAcc = new SWBResourceURLImp(request, this.getResourceBase(), response.getWebPage(), SWBResourceURL.UrlType_ACTION);
+                    this.confirmationActionUrl = serverUrl + urlAcc.setAction("confirming").toString();
+                }               
+                this.sendConfirmationEmail(created);
+                System.out.println("Se creo, el usuario  >>>");
+            }*/
+            nextMode = response.Mode_VIEW;
+        }else if (ACTION_CONFIRMING.equals(action)) {
+            User user = this.activateUser(request);
             StringBuilder credential = new StringBuilder(16);
             
             if (null != user) {
@@ -92,7 +146,7 @@ public class UserRegistry extends GenericResource {
                      Iterator<SWBSessionObject> llist =SWBPortal.getUserMgr().listSessionObjects();
                      while(llist.hasNext()) {
                          SWBSessionObject so = llist.next();
-                         Iterator<Principal> lpri = so.getSubjectByUserRep(userRepo.getId()).getPrincipals().iterator();
+                         Iterator<Principal> lpri = so.getSubjectByUserRep(userRepository.getId()).getPrincipals().iterator();
                          if (lpri.hasNext() && login.equalsIgnoreCase(((User)lpri.next()).getLogin())) {
                              alert = "msg_alreadyLoggedin";
                          }
@@ -127,7 +181,32 @@ public class UserRegistry extends GenericResource {
 //            if (user.isSigned()) {
                 nextMode = "homeRedirect";
 //            }
+        } else if(ACTION_BE_ANNOTATOR.equals(action)||
+                //response.Action_EDIT.equals(action) ||
+                response.Action_ADD.equals(action)){ 
+System.out.println("editar agregar ser");
+//System.out.println(request.getParameter("usrFirstName"));
+//System.out.println(request.getParameter("usrLastName"));
+//System.out.println(request.getParameter("email"));
+
+
+
+
+/**
+ * recibe todos los parametros
+ * 
+ * si es add
+ * 
+ * agraga os basicos
+ * 
+ * 
+
+
+*/
+
+            nextMode = UserRegistry.REGISTER_MODE;
         }
+//System.out.println("termino");        
         response.setMode(nextMode);
     }
 
@@ -178,54 +257,170 @@ public class UserRegistry extends GenericResource {
             UserRegistry.LOG.error("Confirming registry", ioe);
         }
     }
-    
-    private User createProfile(HttpServletRequest request, UserRepository userRepo) {
-        
+    private User createProfile(HttpServletRequest request) {
+//System.out.println("createProfile");        
         User newUser = null;
-        String name = request.getParameter("usrname");
+        String firstName = request.getParameter("usrFirstName");
         String lastName = request.getParameter("usrLastName");
         String email = request.getParameter("email");
-        String password = request.getParameter("pass2");
+        String password = request.getParameter("pass2");        
         String passwordConfirm = request.getParameter("passConf");
         String termsPrivacy = request.getParameter("termsPrivacy");
         String condition = null;
+        //anotador
+        String organ = request.getParameter("usrOrg");
+        String title = request.getParameter("usrTitle");    
+        String inter = request.getParameter("usrInter");
+        String tel = request.getParameter("usrTel");  
         //boolean profCreated = true;
-        
+//System.out.println("1");          
         if (null != password && null != email && !password.isEmpty() &&
-                password.equals(passwordConfirm) && !email.isEmpty() && termsPrivacy.equals("true")) {
+                password.equals(passwordConfirm) && !email.isEmpty() &&
+                null!=termsPrivacy && termsPrivacy.equals("true")) {
             
-            User user = userRepo.getUserByLogin(email);
-            
+            User user = userRepository.getUserByLogin(email);
+//System.out.println("2");              
             if (null == user) {
-                try {
-                    String encryptdPwd;
-                    encryptdPwd = SWBUtils.CryptoWrapper.passwordDigest(password);
-                    newUser = userRepo.createUser();
+                //try {
+//System.out.println("3");                      
+                    //String encryptdPwd;
+                    //encryptdPwd = SWBUtils.CryptoWrapper.passwordDigest(password);
+                    newUser = userRepository.createUser();
                     newUser.setLogin(email);
                     newUser.setPassword(password);  //encryptdPwd
                     newUser.setLanguage("es");
-                    //newUser.setFirstName(name);
-                    //newUser.setLastName(lastName);
+                    if (firstName!=null){
+                        newUser.setFirstName(firstName);
+                    }    
+                    if (lastName!=null){
+                        newUser.setLastName(lastName);
+                    }
                     newUser.setEmail(email);
+//System.out.println("4");                      
                     System.out.println("Contraseña para nuevo usuario: " + password);
                     //newUser.setActive(true);
                     SemanticObject obj = newUser.getSemanticObject();
                     OntModel ont = SWBPlatform.getSemanticMgr().getSchema().getRDFOntModel();
                     obj.getRDFResource().addLiteral(ont.createDatatypeProperty(
                         UserRegistry.DATACRED_URI), password);
-                } catch (Exception e) {
-                    condition = "msg_encryptingError";
-                }
+                    if(organ!=null){
+                        obj.getRDFResource().addLiteral(ont.createDatatypeProperty(
+                            UserRegistry.ORGANITATION_NAME_URI),organ);
+                    }
+                    if(title!=null){
+                        obj.getRDFResource().addLiteral(ont.createDatatypeProperty(
+                            UserRegistry.POSITION_TITLE_URI),title);
+                    }
+                    if(inter!=null){
+                        obj.getRDFResource().addLiteral(ont.createDatatypeProperty(
+                            UserRegistry.AREA_INTEREST_URI),inter);
+                    }
+                    if(tel!=null){
+                        obj.getRDFResource().addLiteral(ont.createDatatypeProperty(
+                            UserRegistry.TELEPHONE_NUMBER_URI),tel);
+                    }
+//System.out.println("4");                      
+                //} catch (Exception e) {
+//System.out.println("5");                      
+                //    condition = "msg_encryptingError";
+                //}
             } else {
+//System.out.println("5");                  
                 condition = "msg_userExists";
             }
         } else if (null != password && null != passwordConfirm && !password.equals(passwordConfirm)) {
+//System.out.println("6");              
             condition = "msg_pwdMismatch";
         }
         if (null != newUser || null != condition) {
+//System.out.println("7");              
             request.setAttribute("condition", condition);
         }
+//System.out.println("8 condition:"+condition);
         return newUser;
+    }
+    
+    private User updateProfile(HttpServletRequest request,User user) {
+//System.out.println("updateProfile");        
+        
+        String firstName = request.getParameter("usrFirstName");
+        String lastName = request.getParameter("usrLastName");
+        String email = request.getParameter("email");
+        String password = request.getParameter("pass2");        
+        String passwordConfirm = request.getParameter("passConf");
+        //String termsPrivacy = request.getParameter("termsPrivacy");
+        String condition = null;
+        //anotador
+        String organ = request.getParameter("usrOrg");
+        String title = request.getParameter("usrTitle");    
+        String inter = request.getParameter("usrInter");
+        String tel = request.getParameter("usrTel");  
+        //boolean profCreated = true;
+//System.out.println("1"+firstName+","+lastName+","+email+","+password+","+passwordConfirm+","+organ+","+title+","+inter+","+tel);  
+        
+        if (null != email &&!email.isEmpty() &&
+                null != firstName &&!firstName.isEmpty() &&
+                null != lastName &&!lastName.isEmpty()){
+                
+                /*&& null != password 
+                null != password && null != email && !password.isEmpty() &&
+                password.equals(passwordConfirm) &&  && termsPrivacy.equals("true")) {*/
+            
+//System.out.println("2");              
+//System.out.println("3");                      
+                    //String encryptdPwd;
+                    //encryptdPwd = SWBUtils.CryptoWrapper.passwordDigest(password);
+                    //user.setLogin(email);
+                    user.setFirstName(firstName);
+                    user.setLastName(lastName);
+                    user.setEmail(email);
+                     //encryptdPwd
+                    //user.setLanguage("es");
+                    
+//System.out.println("4");                      
+                    
+                    //newUser.setActive(true);
+                    SemanticObject obj = user.getSemanticObject();
+                    OntModel ont = SWBPlatform.getSemanticMgr().getSchema().getRDFOntModel();
+                    
+                    if (null != password &&  !password.isEmpty() && password.equals(passwordConfirm)){
+                        user.setPassword(password);
+                        System.out.println("Contraseña para nuevo usuario: " + password);
+                        
+                        Property prprt=ont.createDatatypeProperty(UserRegistry.DATACRED_URI);
+                        user.getSemanticObject().getRDFResource().removeAll(prprt);                        
+                        obj.getRDFResource().addLiteral(prprt, password);
+                    }    
+                    if(organ!=null){
+                        Property prprt=ont.createDatatypeProperty(UserRegistry.ORGANITATION_NAME_URI);
+                        user.getSemanticObject().getRDFResource().removeAll(prprt);                        
+                        obj.getRDFResource().addLiteral(prprt, organ);
+                    }
+                    if(title!=null){
+                        Property prprt=ont.createDatatypeProperty(UserRegistry.POSITION_TITLE_URI);
+                        user.getSemanticObject().getRDFResource().removeAll(prprt);                        
+                        obj.getRDFResource().addLiteral(prprt,title);
+                    }
+                    if(inter!=null){
+                        Property prprt=ont.createDatatypeProperty(UserRegistry.AREA_INTEREST_URI);
+                        user.getSemanticObject().getRDFResource().removeAll(prprt);                        
+                        obj.getRDFResource().addLiteral(prprt, inter);
+                    }
+                    if(tel!=null){
+                        Property prprt=ont.createDatatypeProperty(UserRegistry.TELEPHONE_NUMBER_URI);
+                        user.getSemanticObject().getRDFResource().removeAll(prprt);                        
+                        obj.getRDFResource().addLiteral(prprt,tel );
+                    }                     
+//System.out.println("5");                      
+        } else {
+            condition = "msg_missingData";
+        }
+        if (null != user || null != condition) {
+//System.out.println("7");              
+            request.setAttribute("condition", condition);
+        }
+//System.out.println("8 condition:"+condition);
+        return user;
     }
     
     private void sendConfirmationEmail(User user) {
@@ -282,14 +477,14 @@ public class UserRegistry extends GenericResource {
      * @return true, siempre que se verifique que la cuenta de correo indicada esta 
      *         asociada a un usuario registrado, y que la peticion contiene datos validos.
      */
-    private User activateUser(HttpServletRequest request, UserRepository userRepo) {
+    private User activateUser(HttpServletRequest request) {
         
         String eMail = request.getParameter("account");
         String hash = request.getParameter("thru");
         User fulanito = null;
         
         if (null != eMail && null != hash && !eMail.isEmpty() && !hash.isEmpty()) {
-            fulanito = userRepo.getUserByLogin(eMail);
+            fulanito = userRepository.getUserByLogin(eMail);
             if (null != fulanito) {
                 String legitHash = null;
                 try {
